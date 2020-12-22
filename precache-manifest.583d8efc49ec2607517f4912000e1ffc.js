02a01ec448b08fd381f0dcec85f9205f6b471eb3
lichangnan <changnan666@gmail.com>
2020-12-22 16:12:50 +0800

    Updates

---
+++ precache-manifest.583d8efc49ec2607517f4912000e1ffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe7110decafcab7b08b8817c401d40c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "061a694039c10ec4f33c",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "60cee1ad7ac2f2d224d2",
+    "url": "./static/js/2.9c25c578.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.9c25c578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "061a694039c10ec4f33c",
+    "url": "./static/js/main.27639e2f.chunk.js"
+  },
+  {
+    "revision": "c800e1fef3e2902bed59",
+    "url": "./static/js/runtime-main.7c8a1b72.js"
+  }
+]);
