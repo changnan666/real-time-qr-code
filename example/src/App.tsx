6afa615ef8a581b27a4a99ce6f9a690ea1c019c2
lichangnan <changnan666@gmail.com>
2020-12-22 16:22:22 +0800

    补充信息

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,45 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 import ExampleComponent from 'real-time-qr-code'
 
+const style: React.CSSProperties = {
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  alignItems: 'center'
+}
+
 const App = () => {
-  const [msg, setMsg] = useState('1')
+  const [value, setValue] = useState('Hello World')
+  const [code, setCode] = useState('Hello World')
 
-  useEffect(() => {
-    setTimeout(() => {
-      setMsg(msg + '1')
-    }, 1000)
-  }, [msg])
+  const onClick = () => {
+    setCode(value)
+  }
 
-  return <ExampleComponent text={msg} />
+  return (
+    <div style={style}>
+      <div style={{ margin: '50px auto' }}>
+        <ExampleComponent text={code} />
+      </div>
+      <div>
+        <input
+          type='text'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button onClick={onClick}>更新二维码</button>
+        <button
+          onClick={() => {
+            setCode('https://www.bilibili.com/')
+            setValue('https://www.bilibili.com/')
+          }}
+        >
+          可跳转
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default App
